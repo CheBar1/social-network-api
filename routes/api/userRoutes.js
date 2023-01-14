3b14d39ac960ec0f6f993428927972dcdb148ac2
CheBar1 <chebarclay@live.com.au>
2023-01-14 17:49:55 +1030

    added pseudo code to routes/api/userRoutes.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+
+
+// get all users
+
+
+// get a single user by its _id and populated thought and friend data
+
+
+// post a new user: 
+
+// example data 
+// {
+//     "username": "lernantino",
+//     "email": "lernantino@gmail.com"
+// }
+
+
+// put to update a user by its _id
+
+
+
+// delete to remove a user by its _id - BONUS remove a user's associated thoughts when deleted
+
